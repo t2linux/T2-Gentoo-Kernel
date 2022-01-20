7eade392871617dd562eb6b0fb33069cdb37068e
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-01-20 11:25:54 -0800

    Replaced manual copying of files with make install in package.sh

--- package.sh
+++ package.sh
@@ -14,11 +14,12 @@ mkdir -p $SCRIPT_DIR/lib/modules/linux-t2
 mkdir -p $SCRIPT_DIR/usr/src/linux-t2
 
 echo "==> Creating linux-t2-${KERNEL_VERSION}.tar.xz..."
-cp $SCRIPT_DIR/linux-t2/.config $SCRIPT_DIR/boot/config-${KERNEL_VERSION}
-cp $SCRIPT_DIR/linux-t2/System.map-${KERNEL_VERSION} $SCRIPT_DIR/boot/
-cp $SCRIPT_DIR/linux-t2/vmlinuz-${KERNEL_VERSION} $SCRIPT_DIR/boot/
+#cp $SCRIPT_DIR/linux-t2/.config $SCRIPT_DIR/boot/config-${KERNEL_VERSION}
+#cp $SCRIPT_DIR/linux-t2/System.map-${KERNEL_VERSION} $SCRIPT_DIR/boot/
+#cp $SCRIPT_DIR/linux-t2/vmlinuz-${KERNEL_VERSION} $SCRIPT_DIR/boot/
 cd $SCRIPT_DIR/linux-t2
 INSTALL_MOD_PATH=$SCRIPT_DIR/lib make modules_install
+INSTALL_PATH=$SCRIPT_DIR/boot make install
 tar -cz $SCRIPT_DIR/boot $SCRIPT_DIR/lib > $SCRIPT_DIR/linux-t2-${KERNEL_VERSION}.tar.xz
 
 echo "==> Creating linux-t2-src-${KERNEL_VERSION}.tar.xz..."
